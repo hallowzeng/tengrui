13b7b58e11a6572ec82228e81b4ee940fb041271
ZengWeixiang <1303692620@qq.com>
2019-06-09 16:37:15 +0800

    tr1

---
+++ TengruiGroup/js/header.js
@@ -0,0 +1,28 @@
+layui.define(['jquery', 'element', 'carousel', 'laypage'], function(exports){
+  var $ = layui.jquery
+  ,element = layui.element
+  ,carousel = layui.carousel
+  ,laypage = layui.laypage;
+  
+	//导航切换
+  var btn = $('.nav').find('.nav-list').children('button')
+  ,spa = btn.children('span')
+  ,ul = $('.nav').find('.nav-list').children('.layui-nav');
+  btn.on('click', function(){
+    if(!$(spa[0]).hasClass('spa1')){
+      spa[0].className = 'spa1';
+      spa[1].style.display = 'none';
+      spa[2].className = 'spa3';
+      $('.nav')[0].style.height = 90 + ul[0].offsetHeight + 'px';
+    }else{
+      spa[0].className = '';
+      spa[1].style.display = 'block';
+      spa[2].className = '';
+      $('.nav')[0].style.height = 80 + 'px';
+    }
+  });
+  
+
+ exports('header', {}); 
+});
+
---
+++ TengruiGroup/js/index2.js
@@ -0,0 +1,8 @@
+//改变头部二级导航栏点击事件的背景颜色
+$(document).ready(function(){
+//	$('.layui-nav-child dd').click(function(){
+//		$('.layui-nav-child dd').css('background','#fff');
+//		alert("success!");
+//	})
+
+})
